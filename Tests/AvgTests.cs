dbd14b9d2558ce08db1d9dd6bb0e507c39eeac7b
Dani_Flying_Fire <dani.brand@gmx.de>
2021-03-17 22:03:19 +0100

    added unit test project

---
+++ Tests/AvgTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class AvgTests
+    {
+        [TestMethod]
+        public void NormalAvgTest()
+        {
+
+        }
+    }
+}
