a123817cfba799b1b844c175f5da9ad896e7095d
Dani_Flying_Fire <dani.brand@gmx.de>
2021-03-20 03:41:45 +0100

    added animation

--- AbiCALC/MainWindow.xaml.cs
+++ AbiCALC/MainWindow.xaml.cs
@@ -21,6 +21,14 @@ namespace AbiCALC
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int navMin = 50, navMax = 150;
+        
+        private bool navPanelMovementFinished = true;
+        private bool navPanelMovementAim = true;
+
+        private object navPanelMovementFinishedLock = new object();
+        private object navPanelMovementAimLock = new object();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,10 +36,82 @@ namespace AbiCALC
 
         private void tgl_button_clicked(object sender, RoutedEventArgs e)
         {
+            lock (navPanelMovementAimLock) 
+            {               
+                navPanelMovementAim = !(bool)(sender as ToggleButton).IsChecked;                  
+            }
+            lock (navPanelMovementFinishedLock)
+            {
+                if(navPanelMovementFinished) 
+                {
+                    navPanelMovementFinished = false;
+                    nav_pnl_expand(30, new TimeSpan(0, 0, 2));                  
+                }             
+            }
+            
         }
 
-        async private void nav_pnl_expand()
+        private async void nav_pnl_expand(int fps, TimeSpan duration)
         {
+            float pos = 0f;
+            int frame;
+            int frames = 0;           
+            int maxFrame = (int)(((float)duration.TotalSeconds * fps));
+            lock (navPanelMovementAimLock) 
+            {
+                frame = !navPanelMovementAim ? 0 : maxFrame;
+            }
+            DateTime time = DateTime.Now;
+            while (true) 
+            {
+                
+                lock (navPanelMovementAimLock)
+                {
+                    //calc next frame
+                    bool dir = !navPanelMovementAim;
+                    frame += dir ? 1 : -1;
+
+                    //increment frames count
+                    frames++;
+
+                    //get pos [0;1]
+                    pos = (float)frame / (float)maxFrame;
+
+                    //apply pos
+                    nav_panel.Width = (int)(navMin + map(pos,f) * (navMax - navMin));
+
+                    //break?
+                    if ((frame == (((bool)dir) ? maxFrame : 0)))
+                    {
+                        lock (navPanelMovementFinishedLock)
+                        {
+                            navPanelMovementFinished = true;
+                            break;
+                        }
+                    }
+                }
+
+                //wait
+                int timeNeeded = (int)(DateTime.Now.Subtract(time).TotalMilliseconds);
+
+                int timeExpected = 1000 / fps * frames;
+
+                int timeDelta = timeExpected - timeNeeded;
+                await Task.Delay(Math.Max(timeDelta,0));
+
+            }          
+        }
+
+        private float f(float input) 
+        {
+            return (float)(Math.Exp(input));
+        }
+
+        private float map(float f, Func<float, float> func) 
+        {
+            float deltaY = -func(0f);
+            float scaleY = 1 / (deltaY + func(1f));
+            return scaleY * (deltaY + func(f));
         }
     }
 }
