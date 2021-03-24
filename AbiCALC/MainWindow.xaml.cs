883d873b2ea5258f9e6861cd8d6a98cae140c7cf
simonfranken <77718569+simonfranken@users.noreply.github.com>
2021-03-24 15:37:47 +0100

    Custom close button

--- AbiCALC/MainWindow.xaml.cs
+++ AbiCALC/MainWindow.xaml.cs
@@ -61,89 +61,10 @@ namespace AbiCALC
             profile_window.Visibility = Visibility.Visible;
         }
 
-
-
-        /*private void tgl_button_clicked(object sender, RoutedEventArgs e)
+        private void close_clicked(object sender, MouseButtonEventArgs e)
         {
-            lock (navPanelMovementAimLock) 
-            {               
-                navPanelMovementAim = !(bool)(sender as ToggleButton).IsChecked;                  
-            }
-            lock (navPanelMovementFinishedLock)
-            {
-                if(navPanelMovementFinished) 
-                {
-                    navPanelMovementFinished = false;
-                    nav_pnl_expand(60, new TimeSpan(0, 0, 0, 0, 200));                  
-                }             
-            }
-            */
-
-
-        /*
-        private async void nav_pnl_expand(int fps, TimeSpan duration)
-        {
-            float pos = 0f;
-            int frame;
-            int frames = 0;           
-            int maxFrame = (int)(((float)duration.TotalSeconds * fps));
-            lock (navPanelMovementAimLock) 
-            {
-                frame = !navPanelMovementAim ? 0 : maxFrame;
-            }
-            DateTime time = DateTime.Now;
-            while (true) 
-            {
-                
-                lock (navPanelMovementAimLock)
-                {
-                    //calc next frame
-                    bool dir = !navPanelMovementAim;
-                    frame += dir ? 1 : -1;
-
-                    //increment frames count
-                    frames++;
-
-                    //get pos [0;1]
-                    pos = (float)frame / (float)maxFrame;
-
-                    //apply pos
-                    nav_panel.Width = (int)(navMin + map(pos,f) * (navMax - navMin));
-
-                    //break?
-                    if ((frame == (((bool)dir) ? maxFrame : 0)))
-                    {
-                        lock (navPanelMovementFinishedLock)
-                        {
-                            navPanelMovementFinished = true;
-                            break;
-                        }
-                    }
-                }
-
-                //wait
-                int timeNeeded = (int)(DateTime.Now.Subtract(time).TotalMilliseconds);
-
-                int timeExpected = 1000 / fps * frames;
-
-                int timeDelta = timeExpected - timeNeeded;
-                await Task.Delay(Math.Max(timeDelta,0));
-
-            }          
+            Close();
         }
 
-        private float f(float input) 
-        {
-            return (float)(Math.Exp(input));
-        }
-
-
-        private float map(float f, Func<float, float> func) 
-        {
-            float deltaY = -func(0f);
-            float scaleY = 1 / (deltaY + func(1f));
-            return scaleY * (deltaY + func(f));
-        }
-        */
     }
 }
