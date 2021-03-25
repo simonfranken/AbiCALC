f7c74bcc5ae5a8eb34325650b75a366d5ed9bbb2
Dani_Flying_Fire <dani.brand@gmx.de>
2021-03-26 00:18:50 +0100

    added lookUpTable

---
+++ AbiCALC/lookUpTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbiCALC
+{
+    public class lookUpTable
+    {
+        private static lookUpTable singleton;
+        private List<int> upperBounds = new List<int>();
+
+        private lookUpTable() 
+        {
+            int i = 900;
+            upperBounds.Add(i);
+            i -= 78;
+            upperBounds.Add(i);
+            for (int x = 0; x < 28; x++)
+            {
+                i -= 18;
+                upperBounds.Add(i);
+            }
+        }
+
+        private int _getAbi(int i) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int getAbi(int points) => (singleton ??= new lookUpTable())._getAbi(points);
+
+        public static string getAbiGrade(int abi) => $"{abi / 10},{abi % 10}";
+        
+    }
+}
