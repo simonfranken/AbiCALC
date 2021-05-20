47ffe5a16ae5dbcad826559a8c492f1b36da561d
Kartoffel40 <51762684+Kartoffel40@users.noreply.github.com>
2021-05-20 16:07:20 +0200

    Added minimum Einbringungen in min

--- AbiCALC/mins.cs
+++ AbiCALC/mins.cs
@@ -10,5 +10,39 @@ namespace AbiCALC
     public class mins
     {
         public Dictionary<subjectTypes, int> m = new Dictionary<subjectTypes, int>();
+        
+        public mins()
+        {
+            m[subjectTypes.Deutsch] = 4;
+            m[subjectTypes.Mathe] = 4;
+
+            m[subjectTypes.Sport] = 0;
+            m[subjectTypes.Religion] = 3;
+
+            m[subjectTypes.Geschichte] = 3;
+            m[subjectTypes.Sozialkunde] = 3;
+            
+            m[subjectTypes.Geographie] = 3;
+            m[subjectTypes.Wirtschaft] = 3;
+            m[subjectTypes.Wirtschaftsinformatik] = 0;
+
+            m[subjectTypes.Kunst] = 3;
+            m[subjectTypes.Musik] = 3;
+
+            m[subjectTypes.Englisch] = 1;
+            m[subjectTypes.Franzoesisch] = 1;
+            m[subjectTypes.Latein] = 1;
+            m[subjectTypes.Altgriechisch] = 1;
+            m[subjectTypes.Russisch] = 1;
+            m[subjectTypes.Spanisch] = 1;
+            m[subjectTypes.Tuerkisch] = 1;
+            
+            m[subjectTypes.Biologie] = 1;
+            m[subjectTypes.Physik] = 1;
+            m[subjectTypes.Biophysik] = 1;
+            m[subjectTypes.Astrophysik] = 1;
+            m[subjectTypes.Chemie] = 1;
+            m[subjectTypes.Informatik] = 1;
+        }
     }
 }
