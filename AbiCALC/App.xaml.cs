84890c1fb3f6926df4c289467b3fb01eec4d4c93
Simon Franken <simonfranken03@gmail.com>
2021-07-30 17:31:14 +0200

    fix #7

--- AbiCALC/App.xaml.cs
+++ AbiCALC/App.xaml.cs
@@ -12,6 +12,35 @@ namespace AbiCALC
     /// </summary>
     public partial class App : Application
     {
+
+        public App()
+        {
+            InitializeComponent();
+        }
+        
+        
+        public static Color? selectedColor = null;
+        public static selections.selection newSelection;
+        public static bool newAccountFinished = false;
+        
+        private static data promptNewAccount() 
+        {
+            while(!newAccountFinished)
+                (new windows.newAccount()).ShowDialog();
+            newAccountFinished = false;
+            return new data(newSelection);
+        }
+
+        public static void createNewAccount() 
+        {
+            serialization.database.addNew(promptNewAccount());
+        }
+        public void Close() 
+        {
+            foreach (Window x in Application.Current.Windows) x.Close();
+            serialization.database.close();
+            Application.Current.Shutdown();
+        }
         
     }
 }
--- AbiCALC/resourceDictionaries/styles.xaml.cs
+++ AbiCALC/resourceDictionaries/styles.xaml.cs
@@ -4,6 +4,7 @@ using lib.interfaces;
 using System.Windows.Input;
 using System.Windows.Controls;
 using System.Drawing;
+using AbiCALC;
 
 namespace AbiCALC.resourceDictionaries
 {
@@ -16,9 +17,6 @@ namespace AbiCALC.resourceDictionaries
             InitializeComponent();
         }
         
-        public static Color? selectedColor = null;
-        public static selections.selection newSelection;
-        public static bool newAccountFinished = false;
         private void close_clicked(object sender, MouseButtonEventArgs e)
         {
             ((IWindow)Window.GetWindow((Image)sender)).close_clicked(sender, e);
@@ -31,28 +29,12 @@ namespace AbiCALC.resourceDictionaries
         {
             ((IWindow) Window.GetWindow((Image)sender)).min_clicked(sender, e);
         }
-        public void Close() 
-        {
-            foreach (Window x in Windows) x.Close();
-            serialization.database.close();
-            Shutdown();
-        }
+        
         public ObservableCollection<subjectTypes> subjects;
 
         public ObservableCollection<semester> semesters;
 
-        private static data promptNewAccount() 
-        {
-            while(!newAccountFinished)
-                (new windows.newAccount()).ShowDialog();
-            newAccountFinished = false;
-            return new data(newSelection);
-        }
-
-        public static void createNewAccount() 
-        {
-            serialization.database.addNew(promptNewAccount());
-        }
+        
         
     }
 }
